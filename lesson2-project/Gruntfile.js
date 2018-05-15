40c923305590cb247999a64a9d41c66d0a06ad2f
Wasay Syed <wasay@witinc.com>
2018-05-15 11:05:38 -0400

    Renamed folder

--- lesson2-project/Gruntfile.js
+++ lesson2-project/Gruntfile.js
@@ -21,6 +21,17 @@ module.exports = function(grunt) {
               width: ,
               suffix: ,
               quality:
+              instructors code
+                sizes: [{
+                  width: 1600,
+                  suffix: '_large_2x',
+                  quality: 30
+                },
+                {
+                  width: 800,
+                  suffix: '_large_1x',
+                  quality: 30
+                }]
               */
               name: 'small',
               width: '320',
