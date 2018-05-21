4361d0259cd75e3750d186813cfc9d274f486181
Wasay Syed <wasay@witinc.com>
2018-05-21 01:12:17 -0400

    lesson 2 gruntfile reformated

--- lesson2-project/Gruntfile.js
+++ lesson2-project/Gruntfile.js
@@ -6,98 +6,98 @@
   "grunt responsive_images" re-processes images without removing the old ones
 */
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  grunt.initConfig({
-    responsive_images: {
-      dev: {
-        options: {
-          engine: 'im',
-          sizes: [
-            {
-              /*
-              Change these:
+    grunt.initConfig({
+        responsive_images: {
+            dev: {
+                options: {
+                    engine: 'im',
+                    sizes: [
+                        {
+                            /*
+                            Change these:
 
-              width: ,
-              suffix: ,
-              quality:
-              instructors code
-                sizes: [{
-                  width: 1600,
-                  suffix: '_large_2x',
-                  quality: 30
+                            width: ,
+                            suffix: ,
+                            quality:
+                            instructors code
+                              sizes: [{
+                                width: 1600,
+                                suffix: '_large_2x',
+                                quality: 30
+                              },
+                              {
+                                width: 800,
+                                suffix: '_large_1x',
+                                quality: 30
+                              }]
+                            */
+                            name: 'small',
+                            width: '320',
+                            suffix: '',
+                            quality: 20
+                        },
+                        {
+                            name: 'medium',
+                            width: '640',
+                            suffix: '',
+                            quality: 30
+                        },
+                        {
+                            name: 'large',
+                            width: '1024',
+                            suffix: '',
+                            quality: 40
+                        }
+                    ]
                 },
-                {
-                  width: 800,
-                  suffix: '_large_1x',
-                  quality: 30
+
+                /*
+                You don't need to change this part if you don't change
+                the directory structure.
+                */
+                files: [{
+                    expand: true,
+                    src: ['*.{gif,jpg,png}'],
+                    cwd: 'images_src/',
+                    dest: 'images/'
                 }]
-              */
-              name: 'small',
-              width: '320',
-              suffix: '',
-              quality: 20
-            },
-            {
-              name: 'medium',
-              width: '640',
-              suffix: '',
-              quality: 30
-            },
-            {
-              name: 'large',
-              width: '1024',
-              suffix: '',
-              quality: 40
             }
-          ]
         },
 
-        /*
-        You don't need to change this part if you don't change
-        the directory structure.
-        */
-        files: [{
-          expand: true,
-          src: ['*.{gif,jpg,png}'],
-          cwd: 'images_src/',
-          dest: 'images/'
-        }]
-      }
-    },
-
-    /* Clear out the images directory if it exists */
-    clean: {
-      dev: {
-        src: ['images'],
-      },
-    },
-
-    /* Generate the images directory if it is missing */
-    mkdir: {
-      dev: {
-        options: {
-          create: ['images']
+        /* Clear out the images directory if it exists */
+        clean: {
+            dev: {
+                src: ['images'],
+            },
         },
-      },
-    },
 
-    /* Copy the "fixed" images that don't go through processing into the images/directory */
-    copy: {
-      dev: {
-        files: [{
-          expand: true,
-          src: 'images_src/fixed/*.{gif,jpg,png}',
-          dest: 'images/'
-        }]
-      },
-    },
-  });
+        /* Generate the images directory if it is missing */
+        mkdir: {
+            dev: {
+                options: {
+                    create: ['images']
+                },
+            },
+        },
 
-  grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-mkdir');
-  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+        /* Copy the "fixed" images that don't go through processing into the images/directory */
+        copy: {
+            dev: {
+                files: [{
+                    expand: true,
+                    src: 'images_src/fixed/*.{gif,jpg,png}',
+                    dest: 'images/'
+                }]
+            },
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-mkdir');
+    grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
 
 };
